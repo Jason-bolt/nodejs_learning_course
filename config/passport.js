1240f0967300c5055bee4ab7454a066cc0ffff43
Jason-bolt <appiatujason@gmail.com>
2022-11-09 11:06:23 +0000

    configured passport

---
+++ config/passport.js
@@ -0,0 +1,24 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
+const User = require('../models/User')
+
+module.exports = function (passport) {
+    passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "/auth/google/callback"
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+        console.log(profile)
+    }))
+
+    passport.serializeUser( (user, cb) => {
+        process.nextTick(() => {
+          cb(null, { id: user.id, username: user.username, name: user.name });
+        });
+      });
+      
+      passport.deserializeUser( (user, cb) => {
+        process.nextTick(() => cb(null, user))
+      });
+}
