4a65c5251deef5267c228a79c57fc307b367cc0a
Jason-bolt <appiatujason@gmail.com>
2022-11-12 22:08:47 +0000

    Created stories/edit route

--- routes/stories.js
+++ routes/stories.js
@@ -6,7 +6,7 @@ const Story = require('../models/Story')
 
 
 // @desc    Show add page
-// @route   GET /stories.add
+// @route   GET /stories/add
 router.get('/add', ensureAuth, (req, res) => {
     res.render('stories/add')
 })
@@ -39,6 +39,26 @@ router.get('/', ensureAuth, async (req, res) => {
     } catch (err) {
         console.error(err)
         res.render('error/500')
+    }
+})
+
+// @desc    Show edit page
+// @route   GET /stories/edit/:id
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+    const story = await Story.findOne({
+        _id: req.params.id
+    }).lean()
+
+    if (!story) {
+        return res.render('error/404')
+    }
+
+    if (story.user != req.user._id) {
+        res.redirect('/stories')
+    }else {
+        res.render('stories/edit', {
+            story
+        })
     }
 })
 
